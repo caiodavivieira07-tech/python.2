7b1a1b5ff3ff60cc75016dc76d3f605c3e0ea1af
caiodavivieira07-tech <caiodavivieira07@gmail.com>
2026-08-31 21:32:07 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+v1 = True #booleano
+v2 = 0 #int
+v3 = 10.5 #float
+v4 = "Bola" #string
+
+lista = [v1,v2,v3,v4]
+tupla = [v1,v2,v3,v4] # é uma lista imutável
+
+dicionario = {"id": 1, "nome": "Notebook", "preco": 3500}
